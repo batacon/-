ff694ff300e21db4ed8b84f90fe39f93ed7fede6
kawarabata <kwrbt0404@gmail.com>
2021-05-03 10:44:16 +0900

    gear_inchesから直径の計算を別メソッドに切り出す

--- bycycle/gear.rb
+++ bycycle/gear.rb
@@ -1,9 +1,5 @@
 class Gear
   attr_reader :chainring, :cog
-  # attr_reader :cogで以下が定義される
-  # def cog
-  #   @cog
-  # end
 
   def initialize(chainring, cog)
     @chainring = chainring
@@ -15,7 +11,10 @@ class Gear
   end
 
   def gear_inches
-    # タイヤはリムの周りを囲むので、直径を計算するためには２倍して足す
-    ratio * (rim + (tire * 2))
+    ratio * diameter
+  end
+
+  def diameter
+    rim + (tire * 2)
   end
 end
