5cef36f2985554b036b9ff6b33500a139290693a
kawarabata <kwrbt0404@gmail.com>
2021-05-05 16:25:36 +0900

    新しいサブクラスを簡単に作れることを確認する

--- Bicycle/bicycle.rb
+++ Bicycle/bicycle.rb
@@ -67,3 +67,26 @@ class MountainBike < Bicycle
     '2.1'
   end
 end
+
+class RecumbentBike < Bicycle
+  attr_reader :flag
+
+  def post_initialize(args)
+    @flag = args[:flag]
+  end
+
+  def local_spares
+    { flag: flag }
+  end
+
+  def default_chain
+    '9-speed'
+  end
+
+  def default_tire_size
+    '28'
+  end
+end
+
+bent = RecumbentBike.new(flag: 'tall and orange')
+p bent.spares
