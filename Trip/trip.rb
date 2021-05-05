9eae0847ea3e22dcef1e53155517553060172c72
kawarabata <kwrbt0404@gmail.com>
2021-05-05 20:25:39 +0900

    Schedulableを他のクラスにincludeする

--- Trip/trip.rb
+++ Trip/trip.rb
@@ -35,9 +35,28 @@ class Bicycle
   end
 end
 
+class Vehicle
+  include Schedulable
+
+  def lead_days
+    3
+  end
+end
+
+class Mechanic
+  include Schedulable
+
+  def lead_days
+    4
+  end
+end
+
 require 'date'
 starting = Date.parse('2015/09/04')
 ending = Date.parse('2015/09/10')
 
-b = Bicycle.new
-p b.schedulable?(starting, ending)
+v = Vehicle.new
+p v.schedulable?(starting, ending)
+
+m = Mechanic.new
+p m.schedulable?(starting, ending)
