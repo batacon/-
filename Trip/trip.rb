b7fe09825c7957da4875e670047fa754f17a519d
kawarabata <kwrbt0404@gmail.com>
2021-05-05 20:07:29 +0900

    Scheduleを作成

---
+++ Trip/trip.rb
@@ -0,0 +1,40 @@
+class Schedule
+  def scheduled?(schedulable, start_date, end_date)
+    puts "This #{schedulable.class}" + "is not scheduled\n" +
+      " between #{start_date} and #{end_date}"
+  end
+end
+
+class Bicycle
+  attr_reader :schedule, :size, :chain, :tire_size
+
+  # Scheduleを注入し、初期値を設定する
+  def initialize(args={})
+    @schedule = args[:schedule] || Schedule.new
+    @size = args[:size]
+    @chain = args[:chain]
+    @tire_size = args[:tire_size]
+  end
+
+  # 与えられた期間の間、bicycleが利用可能であればtrueを返す
+  def schedulable?(start_date, end_date)
+    !scheduled?(start_date - lead_days, end_date)
+  end
+
+  # scheduleの答えを返す
+  def scheduled?(start_date, end_date)
+    schedule.scheduled?(self, start_date, end_date)
+  end
+
+  # bicycleがスケジュール可能となるまでの準備日数を返す
+  def lead_days
+    1
+  end
+end
+
+require 'date'
+starting = Date.parse('2015/09/04')
+ending = Date.parse('2015/09/10')
+
+b = Bicycle.new
+p b.schedulable?(starting, ending)
