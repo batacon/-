4151dee839d55993a3f72181bd669fa16ef43540
kawarabata <kwrbt0404@gmail.com>
2021-05-05 11:17:47 +0900

    Revert "typeによる出し分けで遠回り"
    This reverts commit 5528ed2520586f062885008303d573f191388e88.

--- Bicycle/bicycle.rb
+++ Bicycle/bicycle.rb
@@ -1,30 +1,20 @@
 class Bicycle
-  attr_reader :size, :tape_color, :front_shock, :rear_shock
+  attr_reader :size, :tape_color
 
   def initialize(args)
     @size = args[:size]
     @tape_color = args[:tape_color]
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
   end
 
-  # styleの確認は危険な道へ進む一歩
   def spares
-    if style == :road
-      {
-        chain: '10-speed',
-        tire_size: '23', # millimeters
-        tape_color: tape_color
-      }
-    else
-      {
-        chain: '10-speed',
-        tire_size: '2.1', # inches
-        rear_shock: rear_shock
-      }
-    end
+    {
+      chain: '10-speed',
+      tire_size: '23',
+      tape_color: tape_color
+    }
   end
 end
 
-bike = Bicycle.new(size: 'M', tape_color: 'red', front_shock: 'Manitou', rear_shock: 'Fox')
+bike = Bicycle.new(size: 'M', tape_color: 'red')
+bike.size
 bike.spares
